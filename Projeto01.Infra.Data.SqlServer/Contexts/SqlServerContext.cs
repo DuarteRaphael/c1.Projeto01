45ac35605ff7f1308a77eb9513e13518bd4f2698
Raphael Duarte <raphael-duarte@outlook.com>
2024-06-13 01:44:14 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto01.Infra.Data.SqlServer/Contexts/SqlServerContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projeto01.Domain.Entities;
+using Projeto01.Infra.Data.SqlServer.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01.Infra.Data.SqlServer.Contexts
+{
+    /// <summary>
+    /// Classe de contexto do EntityFramework para o SqlServer
+    /// </summary>
+    public class SqlServerContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BDProjeto01;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ContatoConfiguration());
+        }
+
+        public DbSet<Contato>? Contatos { get; set; }
+    }
+}
---
+++ Projeto01.Infra.Data.SqlServer/Contexts/SqlServerContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01.Infra.Data.SqlServer.Contexts
+{
+    /// <summary>
+    /// Classe para executar os comandos do migrations do EntityFramework
+    /// </summary>
+    public class SqlServerContextFactory : IDesignTimeDbContextFactory<SqlServerContext>
+    {
+        public SqlServerContext CreateDbContext(string[] args)
+        {
+            return new SqlServerContext();
+        }
+    }
+}
